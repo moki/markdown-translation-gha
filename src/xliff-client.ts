388be4481eb8ccdc3bf255e170945b6e5cf7d2f7
moki <morozov.kirill.moki@gmail.com>
2023-08-02 17:24:15 +0300

    feat: xliff client

---
+++ src/xliff-client.ts
@@ -0,0 +1,46 @@
+import * as exec from '@actions/exec';
+
+class XLIFFClient {
+    private configured: boolean;
+
+    constructor() {
+        this.configured = false;
+    }
+
+    async configure(): Promise<void> {
+        await exec.exec('npm install @doc-tools/docs -g');
+        this.configured = true;
+    }
+
+    async extract(
+        input: string,
+        output: string,
+        sourceLanguageLocale?: string,
+        targetLanguageLocale?: string
+    ): Promise<void> {
+        if (!(input.length && output.length)) {
+            throw new Error('specify input and output');
+        }
+
+        if (!this.configured) {
+            await this.configure();
+        }
+
+        let sll = 'ru-RU';
+        if (sourceLanguageLocale?.length) {
+            sll = sourceLanguageLocale;
+        }
+
+        let tll = 'en-US';
+        if (targetLanguageLocale?.length) {
+            tll = targetLanguageLocale;
+        }
+
+        const cmd = `yfm xliff extract --input ${input} --output ${output} --sll ${sll} --tll ${tll}`;
+
+        await exec.exec(cmd);
+    }
+}
+
+export {XLIFFClient};
+export default {XLIFFClient};
